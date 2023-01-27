5507e8c627e52741a6fa6366128f3cc8c35eac48
jagruti patil <121845271+jagrutipatil1204@users.noreply.github.com>
2023-01-27 09:59:41 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,19 @@ public class Main {
     {
         int isPresent = 0;
         int isAbsent =1;
+        int dailySalary;
+        int workHours;
+        int salaryPerhours=20;
         int checkAttendence = (int)((Math.random()*10)%2);
         if (checkAttendence==isPresent){
             System.out.println("Employee is present");
+            workHours=8;
         }
         else{
             System.out.println("Employee is Absent");
+            workHours=0;
         }
+        dailySalary=workHours*salaryPerhours;
+        System.out.println("daily Salary is " + dailySalary);
     }
 }
